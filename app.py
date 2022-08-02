4e97fe9727e77204f284b267f99f3747d0064efd
kchang08 <60585948+kchang08@users.noreply.github.com>
2022-08-02 13:59:49 -1000

    Update app.py

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ st.header("This app ...")
 st.markdown("Talk about what is classified...")
 
 # Setup
-# model = load("distracted_driver_cnn.pkl")
+model = load("distracted_driver_cnn.pkl")
 ACTIONS = ['Drinking Coffee', 'Using Mirror', 'Using Radio', 'Attentive Driver']
 
 # Interactive photo upload
-f = st.file_uploader("Upload Image")
-if f is not None:
-  file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
-  image = cv2.imdecode(file_bytes, 1)
-  st.image(image, channels="BGR")
-  scores = model.predict(image)
-  st.write(f"The prediction is: {ACTIONS[scores.argmax()]}")
+#f = st.file_uploader("Upload Image")
+#if f is not None:
+#  file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
+#  image = cv2.imdecode(file_bytes, 1)
+#  st.image(image, channels="BGR")
+#  scores = model.predict(image)
+#  st.write(f"The prediction is: {ACTIONS[scores.argmax()]}")
