492cf09623959fd6af02a2582f10e4b2bb9ea63e
kchang08 <60585948+kchang08@users.noreply.github.com>
2022-08-02 14:09:30 -1000

    Update app.py

--- app.py
+++ app.py
@@ -17,18 +17,27 @@ st.sidebar.info(
 
 # Intro to Distracted Drivers
 st.title("Streamlit Demo: Distracted Drivers")
-st.header("This app ...")
-st.markdown("Talk about what is classified...")
+st.header("This app labbels images of drivers as attentive and non-attentive.")
+st.markdown("The four labels are Drinking Coffee, Using Mirror, Using Radio, and Attentive Driver. Example images of each are shown below.")
+st.image("coffee.jpg")
+st.caption("Drinking Coffee")
+st.image("mirror.jpg")
+st.caption("Using Mirror")
+st.image("radio.jpg")
+st.caption("Using Radio)
+st.image("attentive.jpg")
+st.caption("Attentive Driver")
+
 
 # Setup
 model = load("distracted_driver_cnn.pkl")
 ACTIONS = ['Drinking Coffee', 'Using Mirror', 'Using Radio', 'Attentive Driver']
 
 # Interactive photo upload
-#f = st.file_uploader("Upload Image")
-#if f is not None:
-#  file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
-#  image = cv2.imdecode(file_bytes, 1)
-#  st.image(image, channels="BGR")
-#  scores = model.predict(image)
-#  st.write(f"The prediction is: {ACTIONS[scores.argmax()]}")
+f = st.file_uploader("Upload Image")
+if f is not None:
+  file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
+  image = cv2.imdecode(file_bytes, 1)
+  st.image(image, channels="BGR")
+  scores = model.predict(image)
+  st.write(f"The prediction is: {ACTIONS[scores.argmax()]}")
