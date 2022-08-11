35e09ba8cb7973450d9bbaee17332fba4f1c8a10
kchang08 <60585948+kchang08@users.noreply.github.com>
2022-08-11 13:17:34 -1000

    Update AI_Prediction_Tool.py

--- pages/AI_Prediction_Tool.py
+++ pages/AI_Prediction_Tool.py
@@ -35,7 +35,7 @@ if f is not None:
   image_array = np.asarray(image)
   # Normalize the image
   normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
+  print(normalized_image_array.shape)
   # run the inference
   prediction = model.predict(np.array([normalized_image_array]))
   
