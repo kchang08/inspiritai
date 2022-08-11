cd9e791e0d3f803de77dda144cbcd6ea0d3a4c65
kchang08 <60585948+kchang08@users.noreply.github.com>
2022-08-11 13:13:39 -1000

    Update AI_Prediction_Tool.py

--- pages/AI_Prediction_Tool.py
+++ pages/AI_Prediction_Tool.py
@@ -26,25 +26,18 @@ ACTIONS = ['Drinking Coffee', 'Using Mirror', 'Using Radio', 'Attentive Driver']
 f = st.file_uploader("Upload Image")
 
 if f is not None:
-  
-  
-  data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-  # Replace this with the path to your image
   image=Image.open(f) 
   #resize the image to a 224x224 with the same strategy as in TM2:
   #resizing the image to be at least 224x224 and then cropping from the center
   size = (224, 224)
   image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
   #turn the image into a numpy array
   image_array = np.asarray(image)
   # Normalize the image
   normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-  # Load the image into the array
-  data[0] = normalized_image_array
 
   # run the inference
-  prediction = model.predict(data)
+  prediction = model.predict(normalized_image_array)
   
   st.image(data, channels="BGR")
 
