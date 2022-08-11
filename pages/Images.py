f33cdff15b198b968ae3fee92dd5077d479d15fa
kchang08 <60585948+kchang08@users.noreply.github.com>
2022-08-11 09:54:22 -1000

    Create Images.py

---
+++ pages/Images.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+# Demo
+st.header("Images to Test the Model!")
+st.markdown(
+  """
+  Please feel free to use the below images in the four categories: 
+  Drinking Coffee, Using Mirror, Using Radio, and Attentive Driver.
+  """
+)
